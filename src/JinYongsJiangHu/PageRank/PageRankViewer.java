937b04cd1d5e864babe88f3c02e66762f73bc05f
LF1994 <lfqhrj@qq.com>
2016-07-11 18:15:39 +0800

    PageRankViewer

--- src/JinYongsJiangHu/PageRank/PageRankViewer.java
+++ src/JinYongsJiangHu/PageRank/PageRankViewer.java
@@ -1,7 +1,56 @@
 package JinYongsJiangHu.PageRank;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+import java.io.IOException;
+
 /**
  * Created by lubuntu on 16-7-11.
  */
 public class PageRankViewer {
+
+    static class PageRankViewerMapper extends Mapper<Text, Text, DoubleWritable, Text> {
+        public void map(Text key, Text value, Context context)
+                throws IOException, InterruptedException {
+            double pr = Double.parseDouble(value.toString().split(",")[0]);
+            DoubleWritable keyOut = new DoubleWritable(pr);
+            Text valueOut = key;
+            context.write(keyOut, valueOut);
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            Configuration conf = new Configuration();
+            String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+            if (otherArgs.length < 2) {
+                System.err.println("usage: PageRankViewer <in> <out>");
+                System.exit(2);
+            }
+            Job job = new Job(conf, "JinYongsJiangHu_Job4_PageRank.PageRankViewer");
+            job.setJarByClass(PageRankViewer.class);
+            job.setInputFormatClass(KeyValueTextInputFormat.class);
+            job.setMapperClass(PageRankViewerMapper.class);
+            job.setMapOutputKeyClass(DoubleWritable.class);
+            job.setMapOutputValueClass(Text.class);
+            FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+            FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+            job.waitForCompletion(true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
