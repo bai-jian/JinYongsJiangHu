a476dd326162baec014fe676c84c8c7670819e1e
LF1994 <lfqhrj@qq.com>
2016-07-11 17:56:41 +0800

    PageRankIter

--- src/JinYongsJiangHu/PageRank/PageRankDriver.java
+++ src/JinYongsJiangHu/PageRank/PageRankDriver.java
@@ -8,13 +8,27 @@ public class PageRankDriver {
     private static int times = 10;
 
     public static void main(String[] args) {
-        if (args.length < 2) {
-            System.err.println("usage: PageRankDriver <in> <out>");
+        if (args.length < 4) {
+            System.err.println("usage: PageRankDriver <in> <mid> <out-1> <out-2>");
             System.exit(2);
         }
-        String[] args4GraphBuilder = {args[0], args[1]};
+        System.out.println("PageRank.GraphBuilder");
+        String[] args4GraphBuilder = {args[0], args[1] + "0"};
         GraphBuilder.main(args4GraphBuilder);
-
-
+        System.out.println("PageRank.PageRankIter");
+        String[] args4PageRankIter = {"", ""};
+        for(int i = 0; i < times; ++i) {
+            if (i < times - 1) {
+                args4PageRankIter[0] = args[1] + String.valueOf(i);
+                args4PageRankIter[1] = args[1] + String.valueOf(i + 1);
+            } else {
+                args4PageRankIter[0] = args[1] + String.valueOf(i);
+                args4PageRankIter[1] = args[2];
+            }
+            PageRankIter.main(args4PageRankIter);
+        }
+        System.out.println("PageRank.PageRankViewer");
+        String[] args4PageRankViewer = {args[2], args[3]};
+        PageRankViewer.main(args4PageRankViewer);
     }
 }
--- src/JinYongsJiangHu/PageRank/PageRankIter.java
+++ src/JinYongsJiangHu/PageRank/PageRankIter.java
@@ -1,7 +1,85 @@
 package JinYongsJiangHu.PageRank;
 
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
 /**
  * Created by lubuntu on 16-7-11.
  */
 public class PageRankIter {
+
+    static class PageRankIterMapper extends Mapper<Text, Text, Text, Text> {
+        public void map(Text key, Text value, Context context) throws IOException, InterruptedException {
+            Text keyOut = new Text();
+            Text valueOut = new Text();
+            String[] lists = value.toString().split(",");
+            double pr = Double.parseDouble(lists[0]);
+            for(int i = 1; i < lists.length; ++i) {
+                keyOut.set(lists[i]);
+                valueOut.set("src," + key.toString() + "," + String.valueOf(pr / (lists.length - 1)));
+                context.write(keyOut, valueOut);
+            }
+            int firstCommaIndex = value.toString().indexOf(',');
+            valueOut.set(value.toString().substring(firstCommaIndex + 1));
+            context.write(key, valueOut);
+        }
+    }
+
+    static class PageRankIterReducer extends Reducer<Text, Text, Text, Text> {
+
+        private static double d = 0.85;
+
+        public void reduce(Text key, Iterable<Text> values, Context context)
+                throws IOException, InterruptedException {
+            double pr = 0;
+            StringBuilder strBuilder = new StringBuilder("");
+            for(Text value: values) {
+                String[] lists = value.toString().split(",");
+                if (lists[0].equals("src")) {
+                    pr += d * Double.parseDouble(lists[2]);
+                } else {
+                    pr += (1 - d) / (lists.length - 1);
+                    strBuilder.append(value.toString());
+                }
+            }
+            Text valueOut = new Text(String.valueOf(pr) + "," + strBuilder.toString());
+            context.write(key, valueOut);
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            Configuration conf = new Configuration();
+            if (args.length < 2) {
+                System.err.println("usage: PageRankIter <in> <out>");
+                System.exit(2);
+            }
+            Job job = new Job(conf, "JinYongsJiangHu_Job4_PageRank.PageRankIter");
+            job.setJarByClass(PageRankIter.class);
+            job.setInputFormatClass(KeyValueTextInputFormat.class);
+            job.setMapperClass(PageRankIterMapper.class);
+            job.setMapOutputKeyClass(Text.class);
+            job.setMapOutputValueClass(Text.class);
+            job.setReducerClass(PageRankIterReducer.class);
+            FileInputFormat.addInputPath(job, new Path(args[0]));
+            FileOutputFormat.setOutputPath(job, new Path(args[1]));
+            job.waitForCompletion(true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
