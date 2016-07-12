b00489558fc0a39a8a7748dfcd8af5d366ae4ee1
LF1994 <lfqhrj@qq.com>
2016-07-12 16:34:55 +0800

    Implement LabelPropagation.LabelPropagationDriver

--- src/JinYongsJiangHu/LabelPropagation/LabelPropagationDriver.java
+++ src/JinYongsJiangHu/LabelPropagation/LabelPropagationDriver.java
@@ -1,10 +1,35 @@
 package JinYongsJiangHu.LabelPropagation;
 
+
 /**
  * Created by lubuntu on 16-7-12.
  */
 public class LabelPropagationDriver {
-    public static void main(String[] args) {
 
+    private static int times = 10;
+
+    public static void main(String[] args) {
+        if (args.length < 4) {
+            System.err.println("usage: LabelPropagationDriver <in> <mid> <out-1> <out-2>");
+            System.exit(2);
+        }
+        System.out.println("LabelPropagation.GraphBuilder");
+        String[] args4GraphBuilder = {args[0], args[1] + "0"};
+        GraphBuilder.main(args4GraphBuilder);
+        System.out.println("LabelPropagation.LabelPropagationIter");
+        String[] args4LabelPropagationIter = {"", ""};
+        for(int i = 0; i < times; ++i) {
+            if (i < times - 1) {
+                args4LabelPropagationIter[0] = args[1] + String.valueOf(i);
+                args4LabelPropagationIter[1] = args[1] + String.valueOf(i + 1);
+            } else {
+                args4LabelPropagationIter[0] = args[1] + String.valueOf(i);
+                args4LabelPropagationIter[1] = args[2];
+            }
+            LabelPropagationIter.main(args4LabelPropagationIter);
+        }
+        System.out.println("LabelPropagation.LabelPropagationViewer");
+        String[] args4LabelPropagationViewer = {args[2], args[3]};
+        LabelPropagationViewer.main(args4LabelPropagationViewer);
     }
 }
