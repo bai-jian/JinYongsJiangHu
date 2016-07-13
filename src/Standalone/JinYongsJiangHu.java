7ee5c7ac76cede2a1cf4c98a135cca954487f5e0
LF1994 <lfqhrj@qq.com>
2016-07-13 13:50:03 +0800

    Create the Standalone

---
+++ src/Standalone/JinYongsJiangHu.java
@@ -0,0 +1,17 @@
+package Standalone;
+
+/**
+ * Created by lubuntu on 16-7-13.
+ */
+public class JinYongsJiangHu {
+    
+    public static void main(String[] args) {
+        System.out.println(
+                "**************************************************************" + '\n' +
+                "********** Welcome to JinYong's JiangHu (Standalone) *********" + '\n' +
+                "***********************  金 庸 的 江 湖  **********************" + '\n' +
+                "*** Analyse the relation of characters in JinYong's novels. **" + '\n' +
+                "**************************************************************" + '\n'
+        );
+    }
+}
