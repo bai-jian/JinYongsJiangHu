e67ae592c73d3297676c40dbcb2d4f2302aee3fd
LF1994 <lfqhrj@qq.com>
2016-07-14 20:44:32 +0800

    Set the args

--- src/JinYongsJiangHu/Driver.java
+++ src/JinYongsJiangHu/Driver.java
@@ -17,29 +17,33 @@ public class Driver {
                 "**************************************************************" + '\n'
         );
 
+        if (args.length < 2) {
+            System.err.println("usage: JinYong'sJiangHu <in> <out>");
+            System.exit(2);
+        }
+
         System.out.println("InteractionExtractor begins...");
-        String[] args4InteractionExtractor = {"./input/people_name_list.txt", "./input/novels", "./temp/temp1"};
-        // String[] args4InteractionExtractor = {"./testInput/people_name_list.txt", "./testInput/novels", "./temp/temp1"};
+        String[] args4InteractionExtractor = {args[0] + "/people_name_list.txt", args[0] + "/novels", args[1] + "/temp1"};
         InteractionExtractor.main(args4InteractionExtractor);
         System.out.println("InteractionExtractor finishes successfully...");
 
         System.out.println("CharacterCooccurrence begins...");
-        String[] args4CharacterCooccurrence = {"./temp/temp1", "./temp/temp2"};
+        String[] args4CharacterCooccurrence = {args[1] + "/temp1", args[1] + "/temp2"};
         CharacterCooccurrence.main(args4CharacterCooccurrence);
         System.out.println("CharacterCooccurrence finishes successfully...");
 
         System.out.println("CharacterRelationGraph begins...");
-        String[] args4CharacterRelationGraph = {"./temp/temp2", "./temp/temp3"};
+        String[] args4CharacterRelationGraph = {args[1] + "/temp2", args[1] + "/temp3"};
         CharacterRelationGraph.main(args4CharacterRelationGraph);
         System.out.println("CharacterRelationGraph finishes successfully...");
 
         System.out.println("PageRank begins...");
-        String[] args4PageRank = {"./temp/temp3", "./temp/temp4.", "./temp/temp4", "./temp/temp6-1"};
+        String[] args4PageRank = {args[1] + "/temp3", args[1] + "/temp4.", args[1] + "/temp4", args[1] + "/temp6-1"};
         PageRankDriver.main(args4PageRank);
         System.out.println("PageRank finishes successfully...");
 
         System.out.println("LabelPropagation begins...");
-        String[] args4LabelPropagation = {"./temp/temp3", "./temp/temp5.", "./temp/temp5", "./temp/temp6-2"};
+        String[] args4LabelPropagation = {args[1] + "/temp3", args[1] + "/temp5.", args[1] + "/temp5", args[1] + "/temp6-2"};
         LabelPropagationDriver.main(args4LabelPropagation);
         System.out.println("LabelPropagation finishes successfully...");
     }
